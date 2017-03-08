b83b562f47dfb48faf3afa68b8861c1b8fb6f81d
dmzx <info@dmzx-web.net>
2017-03-08 22:55:43 +0100

    Version 1.0.3

--- acp/acp_donation_module.php
+++ acp/acp_donation_module.php
@@ -26,7 +26,7 @@ class acp_donation_module
 		// Make the $u_action url available in the admin controller
 		$admin_controller->set_page_url($this->u_action);
 
-		switch($mode)
+		switch ($mode)
 		{
 			case 'configuration':
 				// Load a template from adm/style for our ACP page
